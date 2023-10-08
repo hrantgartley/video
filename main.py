8970d3c37b0a5c2648f38db04042e20e8bea3d67
Grant <grant@Grants-MacBook-Pro.local>
2023-10-07 23:51:21 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from video import Video, random
+
+
+def print_msg(msg) -> None:
+    print(f"The given message was {msg}")
+
+
+def bound(lower, upper) -> int:
+    return random.randint(lower, upper)
+
+
+def new_line() -> str:
+    return "\n"
+
+
+def main():
+    video1 = Video(True, 120, [], 4.5)
+    video2 = Video(False, 60, ["comedy", "action"], 3.5)
+    video3 = Video(True, 90, ["drama"], 5.0)
+    video_list = [video1, video2, video3]
+    for _ in range(len(video_list)):
+        print(video_list)
+
+    for key, value in enumerate(video_list):
+        print(key, value)
+
+
+if __name__ == "__main__":
+    main()
